84dfe081d2f3366ebb25a8e6500b3ac654f87815
vitorhugosantana <santana.vitor91@gmail.com>
2022-05-22 19:49:35 -0300

    Adicionado script de cartao de visitas

--- cartao-visitas/index.js
+++ cartao-visitas/index.js
@@ -1,1 +1,14 @@
-console.log ("Este é o meu projeto");
+var prettyjson = require('prettyjson');
+
+var data = {
+  username: 'vitorsantana',
+  url: 'https://github.com/vitorhugosantana',
+  linkedin_account: 'https://www.linkedin.com/in/vitor-santana-765913158/',
+  stack: ['html', 'javascript', 'nodeJs']
+};
+
+var options = {
+  noColor: false
+};
+
+console.log(prettyjson.render(data, options));
